3bd8527230b71c81b60601e9e11972e3bbefa5ec
eet172305 <eet172305@iitd.ac.in>
2018-09-27 16:59:32 +0530

    version-3.0

--- ps1.py
+++ ps1.py
@@ -1,3 +1,64 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+class cell:
+    def __init__(self, char):
+        self.char = char
+        self.top = 0
+        self.down = 0
+	self.left=0
+	self.right=0
+	self.val=0;
+	self.visit=0;
+# find the largest and second largest elemet in the matrix
+def Range(list1): 
+	if( len(list1) >0):
+		largest = list1[0] 
+		largest2 = list1[0]  
+		for item in list1:        
+			if item > largest:  
+				largest = item 
+			elif largest2!=largest and largest2 < item: 
+				largest2 = item       
+		print("Largest element is:", largest*4+1) 
+		print("Second Largest element is:", largest2*4+1) 
+
+m = int(input("Enter k1 "))# enter k1
+n = int(input("Enter k2 "))# enter k2
+matrix = []; columns = []
+for i in range(0,m):
+  matrix += [0]
+# initialize the number of columns
+for j in range (0,n):
+  columns += [0]
+# initialize the matrix
+for i in range (0,m):
+  matrix[i] = columns
+for i in range (0,m):
+	matrix[i]=raw_input().split(" ") # input matrix row from user
+
+array=[]
+# defind structure matrix
+mat = [[0 for x in range(m)] for x in range(n)]
+# initialize the new matrix
+for i in range(m):
+	for j in range(n):
+		mat[i][j]=cell(matrix[i][j])
+# assign the values of top left up down from the original matrix
+for i in range(m):
+	for j in range(n):
+		if (mat[i][j].char == 'S'):
+			if (i-1>=0  and mat[i-1][j].char =='S'):
+				mat[i][j].top=mat[i][j].top+1
+			if (i+1<n  and mat[i+1][j].char =='S'):
+				mat[i][j].down=mat[i][j].down+1
+			if (j-1>=0  and mat[i][j-1].char =='S'):
+				mat[i][j].left=mat[i][j].left+1
+			if (j+1<n  and mat[i][j+1].char =='S'):
+				mat[i][j].right=mat[i][j].right+1
+			mat[i][j].val=min(mat[i][j].left,mat[i][j].right,mat[i][j].top,mat[i][j].down)
+			array.append(mat[i][j].val)
+# function call for the largest and second largest grid
+Range(array) 
+
--- ps2.py
+++ ps2.py
@@ -1,14 +1,15 @@
-k1 = int(input("Enter k1 "))
-k2 = int(input("Enter k2 "))
-k3 = int(input("Enter k3 "))
+k1 = int(input("Enter k1 "))# enter k1
+k2 = int(input("Enter k2 "))# enter k2
+k3 = int(input("Enter k3 "))# enter k3
 name = raw_input("Enter your name: ")
-group1 = ['a', 'b','c','d','e','f','g','h','i']
-group2 = ['j', 'k','l','m','n','0','p','q','r']
-group3 = ['s', 't','u','v','w','x','y','z','_']
+group1 = ['a', 'b','c','d','e','f','g','h','i'] # group 1
+group2 = ['j', 'k','l','m','n','o','p','q','r']# group 3
+group3 = ['s', 't','u','v','w','x','y','z','_']# group 3
 s1=""
 s2=""
 s3=""
 s = ""
+# divide strings based on groups
 for c in name:
 	if c in group1:
     		s1+=c
@@ -17,34 +18,35 @@ for c in name:
 	elif c in group3: 
 		s3+=c
 
-print(s1)
-print(s2)
-print(s3)
-ss1=s1[len(s1)-k1:len(s1)]
-s1=ss1+s1[0:len(s1)-k1]
+org1=s1
+org2=s2
+org3=s3
+#rotate strings
+s1=s1[-k1:]+s1[:-k1]
+#s1=ss1+s1[:-k1]
 
-ss2=s2[len(s2)-k2:len(s2)]
-s2=ss2+s2[0:len(s2)-k2]
+#rotate strings
+s2=s2[-k2:]+s2[:-k2]
+#s2=ss2+s2[:-k2]
 
-ss3=s3[len(s3)-k3:len(s3)]
-s3=ss3+s3[0:len(s3)-k3]
+#rotate strings
+s3=s3[-k3:]+s3[:-k3]
+#s3=ss3+s3[:-k3]
 
-print(s1)
-print (s2)
-print(s3)
-
+#final result
 result=""
 i=0
 j=0
 k=0
+#append strings in final result
 for c in name:
-	if c in group1:
+	if c in s1:
     		result+=s1[i]
 		i=i+1
-	elif c in group2:
+	elif c in s2:
 		result+=s2[j]
 		j=j+1
-	elif c in group3: 
+	elif c in s3: 
 		result+=s3[k]
 		k=k+1
 print(result)
